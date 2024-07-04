e3967d30a98471caee3ef3967949de9cbc811a81
wangjie <wangjie@cwddd.com>
2024-07-04 19:50:51 +0800

    first try

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+os.system("pwd")
+os.system("ls -la")
+print("--------------------------------------")
+
+# 更新文件
+root_path="/home/xlab-app-center"
+launch="https://github.com/lgkkey/openxlab_comfyui_cpu/blob/main/launch.py"
+
+if (os.path.exists(f"{root_path}/launch.py")):
+    os.system(f"rm -rf {root_path}/launch.py")
+
+os.system(f"wget -o launch.py  --no-check-certificate `{launch}`")
+
+    
+os.system("ls -la")
+print("----------------start launch.py----------------------")
+os.chdir(root_path)
+
+os.system("python launch.py")
