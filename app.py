a577ce613520676e253786496a38fc040fba7783
wangjie <wangjie@cwddd.com>
2024-07-04 23:02:17 +0800

    9

--- app.py
+++ app.py
@@ -5,18 +5,21 @@ print("--------------------------------------")
 
 # 更新文件
 root_path="/home/xlab-app-center"
-launch="https://raw.githubusercontent.com/lgkkey/openxlab_comfyui_cpu/main/launch.py"
+launch="https://raw.githubusercontent.com/lgkkey/openxlab_comfyui_cpu"
+
+if (os.path.exists(f"{root_path}/openxlab_comfyui_cpu")):
+    print("openxlab_comfyui_cpu exists")
+    os.system(f"rm -rf {root_path}/openxlab_comfyui_cpu")
+
+print(f"git clone {launch}")
+os.system(f"git clone {launch}")
 
 if (os.path.exists(f"{root_path}/launch.py")):
     print("launch.py exists")
     os.system(f"rm -rf {root_path}/launch.py")
-
-print(f"wget -c -o launch.py  --no-check-certificate {launch}")
-os.system(f"wget -c -o launch.py  --no-check-certificate {launch}")
-
-if (os.path.exists(f"{root_path}/launch.py")):
-    print("launch.py update success")
+    os.system(f"cp {root_path}/openxlab_comfyui_cpu/launch.py {root_path}/launch.py")
     os.system(f"cat {root_path}/launch.py")
+    print("launch.py update success")
     print("--------------------------------------")
     
 os.system("ls -la")
